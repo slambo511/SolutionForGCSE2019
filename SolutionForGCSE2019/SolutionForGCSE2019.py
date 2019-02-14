c11493e84763e2874bc1a127b761e74aa9d7c972
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-02-14 23:23:44 +0000

    start

--- SolutionForGCSE2019/SolutionForGCSE2019.py
+++ SolutionForGCSE2019/SolutionForGCSE2019.py
@@ -1,1 +1,12 @@
+'''
+Herein lay the solutions I have come up with for the 2019 Spec GCSE coding challenges
+THESE ARE MINE
+I WILL fail anyone who copies and pastes my code, it is for learning ONLY
+If you need help, ask me, Google or your buddy - DO NOT COPY IT.
+You will note that each solution has the SPEC right in there to help you see what is left to do
+THIS is what comments are for, use them (It also shows me you understand what your code is 
+doing)
 
+NOTE
+The login file for all the games is SHARED
+'''
