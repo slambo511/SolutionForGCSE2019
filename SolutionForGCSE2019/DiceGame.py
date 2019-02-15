b22b77720b7afad0dac50c5bcbd48b2dbb005e97
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-02-15 00:19:38 +0000

    added initial login blurb -- need password obfuscation

--- SolutionForGCSE2019/DiceGame.py
+++ SolutionForGCSE2019/DiceGame.py
@@ -1,1 +1,15 @@
+# Imports always go first
+import csv
+import random
+import msvcrt
 
+# 1. Allows two players to enter their details, which are then authenticated to ensure that they are
+# authorised players.
+
+# Have player one login
+player_one_name = input("Player one, enter your name: ")
+player_one_pw = input("Player one, enter your password: ")
+
+# Have player two login
+player_two_name = input("Player one, enter your name: ")
+player_two_pw = input("Player one, enter your password: ")
